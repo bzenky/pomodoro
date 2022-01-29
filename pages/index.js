e3c0ec3a58dc150fb4eaa95dbf873d5c409b6458
Jean Lima <jeanlima.dev@gmail.com>
2022-01-29 18:36:08 -0300

    time (start/pause/reset) function/structure

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,64 @@
+import { useEffect, useRef, useState } from 'react'
+
 import {
   Box,
   Button,
   Container,
   Heading,
+  HStack,
   Text,
 } from '@chakra-ui/react'
 
+import { Duration } from 'luxon'
+
 export default function Home() {
+  let initialTimer = Duration.fromObject({ minutes: 45, seconds: 0 })
+
+  const [timer, setTimer] = useState(initialTimer)
+  const [pause, setPause] = useState(false)
+  const [buttonDescription, setButtonDescription] = useState(true)
+
+  let intervalRef = useRef()
+
+  const decreaseNum = () => {
+    if (timer == 0) {
+      setButtonDescription(true)
+      return
+    } else {
+      return setTimer(prev => prev.minus({ seconds: 1 }))
+    }
+  }
+
+  const handleClick = () => {
+    if (pause) {
+      setButtonDescription(true)
+      clearInterval(intervalRef.current)
+    } else {
+      setButtonDescription(false)
+      intervalRef.current = setInterval(decreaseNum, 1000)
+    }
+    setPause(prev => !prev)
+  }
+
+  const resetTimer = () => {
+    handleClick()
+    setTimer(initialTimer)
+    clearInterval(intervalRef.current)
+    
+    if (!pause) {
+      setButtonDescription(true)
+      setPause(prev => !prev)
+    }
+  }
+
+
+  useEffect(() => {
+    if (timer == 0) {
+      clearInterval(intervalRef.current)
+      setButtonDescription(true)
+    }
+  }, [timer])
+
   return (
     <Container align="center">
       <Box mt="50" >
@@ -25,16 +77,18 @@ export default function Home() {
         p="8"
       >
         <Text fontSize="7xl" mb="4" color='gray.600'>
-          45:00
+          {timer.toFormat('mm:ss')}
         </Text>
 
-        <Button size="lg" colorScheme="red">
-          Start
-        </Button>
+        <HStack spacing='8' >
+          <Button size="lg" colorScheme="red" onClick={handleClick}>
+            {buttonDescription ? 'Start' : 'Pause'}
+          </Button>
 
-        <Button size="lg" colorScheme="yellow" mt="4">
-          Pause
-        </Button>
+          <Button size="lg" colorScheme="yellow" mt="4" onClick={resetTimer}>
+            Reset
+          </Button>
+        </HStack>
       </Box>
     </Container>
   )
