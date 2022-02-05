e651f19c190609fe4c8ca4176bcf1ce3debde1dd
Zenky <bzenky@gmail.com>
2022-02-05 09:22:19 -0300

    Structure revamp, chakra changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,16 +13,19 @@ import { Duration } from 'luxon'
 import useSound from 'use-sound'
 
 export default function Home() {
-  let initialTimer = Duration.fromObject({ minutes: 0, seconds: 0 })
+  const [focusDuration, setFocusDuration] = useState(25)
+  const [cycle, setCycle] = useState(1)
+  const [cycleState, setCycleState] = useState('focus')
+
+  let initialTimer = Duration.fromObject({ minutes: focusDuration })
+
+  const [buttonDescription, setButtonDescription] = useState(true)
+  const [timer, setTimer] = useState(initialTimer)
+  const [pause, setPause] = useState(true)
 
   const [alarm] = useSound('/alarm.wav', { interrupt: true })
-  const [reset] = useSound('/reset.wav', { interrupt: true })
+  const [reset] = useSound('/reset.wav', { interrupt: true, volume: 0.6 })
   const [clock] = useSound('/clock.wav', { interrupt: true })
-
-  const [cycle, setCycle] = useState(0)
-  const [timer, setTimer] = useState(initialTimer)
-  const [pause, setPause] = useState(false)
-  const [buttonDescription, setButtonDescription] = useState(true)
 
   let intervalRef = useRef()
 
@@ -38,12 +41,12 @@ export default function Home() {
       }
     }
 
-    if (pause) {
+    if (!pause) {
       setButtonDescription(true)
       clearInterval(intervalRef.current)
     } else {
       setButtonDescription(false)
-      intervalRef.current = setInterval(decreaseNum, 3)
+      intervalRef.current = setInterval(decreaseNum, 1000)
     }
     setPause(prev => !prev)
   }
@@ -52,55 +55,75 @@ export default function Home() {
     setButtonDescription(true)
     setTimer(initialTimer)
     setCycle(0)
+    setPause(true)
+    setCycleState('focus')
     clearInterval(intervalRef.current)
     reset()
-
-    setPause(prev => !prev)
   }
 
   const cycles = (duration, cycle) => {
-    clearInterval(intervalRef.current)
     setButtonDescription(true)
-    setTimer(Duration.fromObject({ minutes: duration, seconds: 0 }))
+    setTimer(Duration.fromObject({ minutes: duration }))
     setCycle(cycle)
+    setPause(true)
+    clearInterval(intervalRef.current)
   }
 
   useEffect(() => {
+    if (timer == 0) {
+      alarm()
+    }
+
     if (timer == 0 && cycle % 2 === 0) {
-      cycles(5, cycle += 1)
+      cycles(focusDuration, cycle += 1)
+      setCycleState('focus')
     } else if (timer == 0 && cycle < 7) {
-      cycles(1, cycle += 1)
+      cycles(5, cycle += 1)
+      setCycleState('shortBreak')
     } else if (timer == 0 && cycle === 7) {
-      cycles(3, 0)
+      cycles(15, 0)
+      setCycleState('longBreak')
     }
   }, [timer])
 
   return (
-    <Container align="center" maxW='container.md'>
-      <Box mt="70" >
-        <Heading fontSize='6xl' color='red.400'>Pomodoro</Heading>
-        <Text mt='4' fontSize='2xl'>Helping you achieve the most of yourself!</Text>
+    <Container
+      align="center"
+      maxW='100vw'
+      width='100%'
+      height='100vh'
+      bg=
+      {
+        cycleState == 'focus'
+          ? 'red.100'
+          : cycleState == 'shortBreak'
+            ? 'blue.100'
+            : 'blue.200'
+      }
+      transition="background-color 300ms linear"
+    >
+      <Box pt="70" >
+        <Heading as='h1' pb='2' fontSize={['5xl', '6xl']} color='red.500'>Pomodoro</Heading>
+        <Text mt='4' fontSize={['xl', '2xl']} >Helping you achieve the most of yourself!</Text>
       </Box>
 
       <Box
-        bg={cycle % 2 == 0 ? 'blue.50' : 'red.50'}
         display='flex'
         flexDirection='column'
         justifyContent='center'
         alignItems='center'
-        mt="8"
-        p="16"
+        maxW='sm'
       >
-        <Text fontSize="7xl" mb="4" color='gray.600'>
+        <Text fontSize={['7xl', '8xl']} my="8" color='gray.600' fontWeight='500'>
           {timer.toFormat('mm:ss')}
         </Text>
 
         <HStack spacing='8' >
-          <Button size="lg" width='100px' colorScheme="red" onClick={handleClick}>
+          <Button size="lg" width='115px' colorScheme="red" onClick={handleClick}>
             {buttonDescription ? 'Start' : 'Pause'}
           </Button>
 
-          <Button size="lg" width='100px' colorScheme="yellow" onClick={resetTimer}>
+          <Button size="lg" width='115px' colorScheme="yellow" onClick={resetTimer}>
             Reset
           </Button>
         </HStack>
