f60aa51098eb99fdeaa1d0b8773d658e06ea5bfb
Jean Lima <jeanlimadev@gmail.com>
2022-02-06 15:53:46 -0300

    reset cycle fix, short/long break state, dynamic title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from 'react'
+import Head from 'next/head'
 
 import {
   Box,
@@ -14,6 +15,8 @@ import useSound from 'use-sound'
 
 export default function Home() {
   const [focusDuration, setFocusDuration] = useState(25)
+  const [shortBreakDuration, setShortBreakDuration] = useState(5)
+  const [longBreakDuration, setLongBreakDuration] = useState(15)
   const [cycle, setCycle] = useState(1)
   const [cycleState, setCycleState] = useState('focus')
 
@@ -54,7 +57,7 @@ export default function Home() {
   const resetTimer = () => {
     setButtonDescription(true)
     setTimer(initialTimer)
-    setCycle(0)
+    setCycle(1)
     setPause(true)
     setCycleState('focus')
     clearInterval(intervalRef.current)
@@ -78,56 +81,61 @@ export default function Home() {
       cycles(focusDuration, cycle += 1)
       setCycleState('focus')
     } else if (timer == 0 && cycle < 7) {
-      cycles(5, cycle += 1)
+      cycles(shortBreakDuration, cycle += 1)
       setCycleState('shortBreak')
     } else if (timer == 0 && cycle === 7) {
-      cycles(15, 0)
+      cycles(longBreakDuration, 0)
       setCycleState('longBreak')
     }
   }, [timer])
 
   return (
-    <Container
-      align="center"
-      maxW='100vw'
-      width='100%'
-      height='100vh'
-      bg=
-      {
-        cycleState == 'focus'
-          ? 'red.100'
-          : cycleState == 'shortBreak'
-            ? 'blue.100'
-            : 'blue.200'
-      }
-      transition="background-color 300ms linear"
-    >
-      <Box pt="70" >
-        <Heading as='h1' pb='2' fontSize={['5xl', '6xl']} color='red.500'>Pomodoro</Heading>
-        <Text mt='4' fontSize={['xl', '2xl']} >Helping you achieve the most of yourself!</Text>
-      </Box>
-
-      <Box
-        display='flex'
-        flexDirection='column'
-        justifyContent='center'
-        alignItems='center'
-        maxW='sm'
+    <>
+      <Head>
+          <title>PomoPomo - {timer.toFormat('mm:ss')}</title>
+        </Head>
+      <Container
+        align="center"
+        maxW='100vw'
+        width='100%'
+        height='100vh'
+        bg=
+        {
+          cycleState == 'focus'
+            ? 'red.100'
+            : cycleState == 'shortBreak'
+              ? 'blue.100'
+              : 'blue.200'
+        }
+        transition="background-color 300ms linear"
       >
-        <Text fontSize={['7xl', '8xl']} my="8" color='gray.600' fontWeight='500'>
-          {timer.toFormat('mm:ss')}
-        </Text>
+        <Box pt="70" >
+          <Heading as='h1' pb='2' fontSize={['5xl', '6xl']} color='red.500'>Pomodoro</Heading>
+          <Text mt='4' fontSize={['xl', '2xl']} >Helping you achieve the most of yourself!</Text>
+        </Box>
 
-        <HStack spacing='8' >
-          <Button size="lg" width='115px' colorScheme="red" onClick={handleClick}>
-            {buttonDescription ? 'Start' : 'Pause'}
-          </Button>
+        <Box
+          display='flex'
+          flexDirection='column'
+          justifyContent='center'
+          alignItems='center'
+          maxW='sm'
+        >
+          <Text fontSize={['7xl', '8xl']} my="8" color='gray.600' fontWeight='500'>
+            {timer.toFormat('mm:ss')}
+          </Text>
 
-          <Button size="lg" width='115px' colorScheme="yellow" onClick={resetTimer}>
-            Reset
-          </Button>
-        </HStack>
-      </Box>
-    </Container>
+          <HStack spacing='8' >
+            <Button size="lg" width='115px' colorScheme="red" onClick={handleClick}>
+              {buttonDescription ? 'Start' : 'Pause'}
+            </Button>
+
+            <Button size="lg" width='115px' colorScheme="yellow" onClick={resetTimer}>
+              Reset
+            </Button>
+          </HStack>
+        </Box>
+      </Container>
+    </>
   )
 }
