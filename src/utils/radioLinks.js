0ba845a78832c2f47f1e7f95650c13e1bb3b3957
Zenky <bzenky@gmail.com>
2022-07-12 19:34:00 -0300

    fix: radio links

--- src/utils/radioLinks.js
+++ src/utils/radioLinks.js
@@ -2,30 +2,26 @@
 export const radioLinks = [
     {
         name: 'Abao',
-        url: 'https://www.youtube.com/embed/kgx4WGK0oNU?autoplay=1&mute=1'
+        url: 'https://www.youtube.com/embed/kgx4WGK0oNU?autoplay=1'
     },
     {
         name: 'Chill Drive',
-        url: 'https://www.youtube.com/embed/t2pU3iTkv3w?autoplay=1&mute=1'
+        url: 'https://www.youtube.com/embed/t2pU3iTkv3w?autoplay=1'
     },
     {
         name: 'Chilled Empire',
-        url: 'https://www.youtube.com/embed/5MNb7DzIStw?autoplay=1&mute=1'
+        url: 'https://www.youtube.com/embed/5MNb7DzIStw?autoplay=1'
     },
     {
         name: 'Lofi Geek Coffee',
-        url: 'https://www.youtube.com/embed/ORBwkXsUNEs?autoplay=1&mute=1'
+        url: 'https://www.youtube.com/embed/ORBwkXsUNEs?autoplay=1'
     },
     {
         name: 'Lofi Girl',
-        url: 'https://www.youtube.com/embed/5qap5aO4i9A?autoplay=1'
+        url: 'https://www.youtube.com/embed/jfKfPfyJRdk?autoplay=1'
     },
     {
         name: 'Lofi Vibes Chill',
-        url: 'https://www.youtube.com/embed/chWWLsSAqYU?autoplay=1&mute=1'
-    },
-    {
-        name: 'StudyMD Lofi Jazz',
-        url: 'https://www.youtube.com/embed/uxR_sTZnBtg?autoplay=1&mute=1'
+        url: 'https://www.youtube.com/embed/chWWLsSAqYU?autoplay=1'
     }
 ]
